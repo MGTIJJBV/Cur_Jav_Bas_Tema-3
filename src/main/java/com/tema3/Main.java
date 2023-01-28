39639705501ea4b065331ebf1ebd5e8b536c5c1f
iscjj <iscjjbv@outlook.com>
2023-01-28 11:36:16 -0600

    Initial commit

---
+++ src/main/java/com/tema3/Main.java
@@ -0,0 +1,12 @@
+package com.tema3;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] nombres  = {"Sr.", "Manuel", "Alberto", "Bernal", "Espiricueta"};
+
+        for(String nombre : nombres){
+            System.out.print(nombre + " ");
+        }
+
+    }
+}
